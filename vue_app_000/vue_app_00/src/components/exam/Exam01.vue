970b096aa0190d37b6168134420944f64d18e034
sa-ling <1527296894@qq.com>
2020-06-19 22:57:38 +0800

    create exam01 and exam02

---
+++ vue_app_000/vue_app_00/src/components/exam/Exam01.vue
@@ -0,0 +1,17 @@
+<template>
+   <div>
+       <h1>src/components/exam/Exam01.vue</h1>
+       <h1>自定义组件Exam组件</h1>
+   </div>
+</template>
+<script>
+  //#调用短消息组件
+  export default {
+      created(){
+          //vue组件生命周期事件,表示当前组件创建成功
+          //立即调用函数
+          this.$toast({message:"添加成功"});
+          //http://127.0.0.1:5050/#/Exam01
+      }
+  }
+</script>
---
+++ vue_app_000/vue_app_00/src/components/exam/Exam02.vue
@@ -0,0 +1,23 @@
+<template>
+ <div>
+      <h2>Exam02组件</h2>
+      <button @click="handle1">点击按钮显示</button>
+  </div>
+</template>
+<script>
+export default {
+    methods:{
+        handle1(){
+            //按钮事件处理函数
+            // console.log(123);
+            this.$toast(
+                {
+                    message:'删除成功',//消息内容
+                    position:'bottom',//消息位置
+                    iconClass:'iconfont icon-chenggong1'//显示图标
+                });
+        }
+    }
+    
+}
+</script>
