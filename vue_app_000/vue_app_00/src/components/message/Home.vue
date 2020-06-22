dff40d2a8ca2d9127379b8b4f6c12a6d286b8670
sa-ling <1527296894@qq.com>
2020-06-22 20:02:27 +0800

    update Fa09.vue and Sub09.vue add page-tabbar

--- vue_app_000/vue_app_00/src/components/message/Home.vue
+++ vue_app_000/vue_app_00/src/components/message/Home.vue
@@ -1,9 +1,15 @@
 <template>
-   <div>
-       <!--父组件-->
-       <h1>Home.vue</h1>
-       <!--3.调用子组件-->
-       <titlebar></titlebar>
+   <div class="page-tabbar">
+       <div class="page-wrap">
+            <!--父组件-->
+            <!-- <h1>父组件</h1> -->
+            <!--3.调用子组件-->
+            <titlebar leftTitle="微信(111)"
+            :rightFirstImg="require('../../assets/ic_search.png')"
+            :rightSecondImg="require('../../assets/ic_add.png')"
+            :search="mysearch"
+            :add3="myadd3"></titlebar>
+       </div>
    </div>
 </template>
 <script>
@@ -13,6 +19,11 @@ export default{
     //2.注册子组件
     components:{
         "titlebar":TitleBar
+    },
+    methods:{
+        //父组件定义函数,将其传递子组件
+        mysearch(){console.log("搜索")},
+        myadd3(){console.log("添加")}
     }
 }
 </script>
--- vue_app_000/vue_app_00/src/components/message/common/TitleBar.vue
+++ vue_app_000/vue_app_00/src/components/message/common/TitleBar.vue
@@ -1,6 +1,55 @@
 <template>
-  <div>
-      <h1>子组件</h1>
+  <div class="page-head">
+     <!--组件：顶部导航条子组件-->
+     <!--左侧文字-->
+     <span>{{leftTitle}}</span>
+     <!--右侧两张图片-->
+     <div class="right-head">
+         <div @click="search" class="searchdiv">
+             <img :src="rightFirstImg" style="width:25px"/>
+         </div>
+         <div @click="add3" class="searchdiv" style="margin-left:20px">
+             <img :src="rightSecondImg" style="width:25px;margin-right:18px"/>
+         </div>
+     </div>
   </div>
 </template>
-
+<script>
+export default{
+    //声明接收父组件的数据
+    props:{
+        leftTitle:{default:""},//1.左侧文字
+        rightFirstImg:{default:""},//2:第一张图片
+        rightSecondImg:{default:""},//3.第二张图片
+        search:{type:Function},//4.事件处理函数
+        add3:{type:Function}//5.事件处理函数 搜索 添加
+    }
+}
+</script>
+<style scoped>
+ /*1:最外层父元素*/
+ .page-head{
+   display: flex;/*当前父元素弹性布局*/
+   position: fixed;/*固定定位*/
+   z-index: 999;/*浮动所有元素上方*/
+   width:100%;
+   justify-content: space-between;/*两端对齐*/
+   align-items: center;/*垂直居中*/
+   background-color:#393a3e;/*灰色背景*/
+   padding-left:7px;  /*内边距*/
+   padding-right:7px;
+   height:48px;       /*高度*/
+   color:#fff;      /*文字颜色*/
+   font-size:18px;
+ }
+ /*2:右侧图片区域*/
+ .right-head{
+   display: flex;
+ }
+ /*3:图片div*/
+ .searchdiv{    /*图片外层父元素*/
+   display: flex;       /*弹性布局*/
+   align-items: center; /*垂直居中*/
+   height:48px;
+ }
+</style>
