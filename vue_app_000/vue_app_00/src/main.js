919bbd79191a682f88488a3bb9904e1fc5e9a727
sa-ling <1527296894@qq.com>
2020-07-02 23:20:48 +0800

    create seeting axios

--- vue_app_000/vue_app_00/src/main.js
+++ vue_app_000/vue_app_00/src/main.js
@@ -14,6 +14,17 @@ Vue.use(MintUI);
 //1.4:引入图标字体文件
 import "./font/iconfont.css"
 
+//配置axios
+//1.5：引入axios库
+import axios from "axios"
+//1.6:配置请求时保存session信息
+axios.defaults.withCredentials=true;
+//1.7：配置请求基础路径
+axios.defaults.baseURL="http://127.0.0.1:8080/";
+//注意：地址栏请输必须
+//http://127.0.0.1:8080/
+//1.8：注册
+Vue.prototype.axios=axios;
 
 new Vue({
   router,
